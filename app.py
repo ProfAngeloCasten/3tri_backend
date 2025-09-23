597e5ba2fb4f594949be65fe0bf667056e539ff2
ProfAngeloCasten <135132224+ProfAngeloCasten@users.noreply.github.com>
2025-09-23 07:06:19 -0300

    Fix: Rodar Servidor

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os  # Adicione esta importação
 
 app = Flask(__name__)
 
@@ -6,6 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+# Apenas para desenvolvimento local (remova ou comente isso para o Render)
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
