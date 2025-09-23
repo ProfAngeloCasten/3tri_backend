989c4eb1f7e345deac075d121a90f6aecbf85275
ProfAngeloCasten <135132224+ProfAngeloCasten@users.noreply.github.com>
2025-09-23 07:48:11 -0300

    Add: Função login e login.html

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-       return render_template('index.html')  # Em vez de return 'Hello, World!'
+       return render_template('index.html')
+       
+@app.route('/login')
+def renderizar_login():
+    return render_template('login.html')
 
-   # O resto permanece igual (incluindo o if __name__ para local)
-   
-# Apenas para desenvolvimento local (remova ou comente isso para o Render)
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
